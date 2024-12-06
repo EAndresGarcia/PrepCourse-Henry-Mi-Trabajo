0cd05cb26085ca724e86e039e914a0ddfab6849e
EAndresGarcia <garciaernestoandres7@gmail.com>
2024-12-06 02:29:17 -0300

    Clase 5 de JS

---
+++ C5 JS/operadores.js
@@ -0,0 +1,26 @@
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto (es el resto de una división, lo que sobra en una división)
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
+
+//Adición  y Strings
+
+console.log('Hola' + 'Camilo');
+//Pero además también se puede hacer con variables. Ej:
+var nombre = 'Camilo';
+var saludo = 'Hola ' + nombre;
+
+console.log(saludo);
---
+++ C5 JS/variables.js
@@ -0,0 +1,25 @@
+var cajon;
+
+// ¿Qué acabamos de hacer?
+// Definimos una variable y le asignamos un valor
+// ¿Qué es una Variable? Son contenedores o recipientes
+
+
+// String
+var bootcamp = 'Henry';
+
+// Números
+var numeroEntero = 17;
+var numeroNegativo = -4;
+var numeroDecimal = 11.75;
+
+// Booleanos
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+// Undefined
+var cajon1;
+console.log(cajon1);
+
+// Null
+var sinNada = null;
